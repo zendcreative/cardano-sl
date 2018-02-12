f82175d92789bd83ebd5c3cfd718d160b019c8ea
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-02-12 23:32:14 +0300

    Bump default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7217,10 +7217,10 @@ inherit (pkgs) mesa;};
         ({ mkDerivation, aeson, base, cardano-sl-binary, cardano-sl-core
          , cardano-sl-crypto, cardano-sl-db, cardano-sl-infra
          , cardano-sl-lrc, cardano-sl-networking, cardano-sl-util, conduit
-         , cpphs, ether, formatting, generic-arbitrary, lens, log-warper
-         , lrucache, mmorph, mtl, QuickCheck, reflection, resourcet
-         , rocksdb-haskell-ng, safe-exceptions, serokell-util, stdenv
-         , text-format, time, transformers, universum, unliftio
+         , containers, cpphs, ether, formatting, generic-arbitrary, lens
+         , log-warper, lrucache, mmorph, mtl, QuickCheck, reflection
+         , resourcet, rocksdb-haskell-ng, safe-exceptions, serokell-util
+         , stdenv, text-format, time, transformers, universum, unliftio
          , unordered-containers
          }:
          mkDerivation {
@@ -7230,10 +7230,11 @@ inherit (pkgs) mesa;};
            libraryHaskellDepends = [
              aeson base cardano-sl-binary cardano-sl-core cardano-sl-crypto
              cardano-sl-db cardano-sl-infra cardano-sl-lrc cardano-sl-networking
-             cardano-sl-util conduit ether formatting generic-arbitrary lens
-             log-warper lrucache mmorph mtl QuickCheck reflection resourcet
-             rocksdb-haskell-ng safe-exceptions serokell-util text-format time
-             transformers universum unliftio unordered-containers
+             cardano-sl-util conduit containers ether formatting
+             generic-arbitrary lens log-warper lrucache mmorph mtl QuickCheck
+             reflection resourcet rocksdb-haskell-ng safe-exceptions
+             serokell-util text-format time transformers universum unliftio
+             unordered-containers
            ];
            libraryToolDepends = [ cpphs ];
            doHaddock = false;
