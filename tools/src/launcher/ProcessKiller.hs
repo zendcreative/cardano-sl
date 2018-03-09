b3aa23a1db4b13c2e957cd252aa576fff24de2b8
DanielRrr <chelsea2005joecole@icloud.com>
2018-03-09 23:29:18 +0300

    [CSL-1773] added test launcher
    fix
    
    fix
    
    added extra dep
    
    updated commit for Win32
    
    erased rubbish
    
    erased rubbish
    
    fixed
    
    fix
    
    fix

--- tools/src/launcher/ProcessKiller.hs
+++ tools/src/launcher/ProcessKiller.hs
@@ -11,32 +11,25 @@ import System.Process.Internals ( withProcessHandle
                                 , ProcessHandle__(..)
                                 , ProcessHandle )
 
-#if defined(OS_WINDOWS)
-import System.Win32.Process (terminateProcessById)
+#ifdef mingw32_HOST_OS
 import System.Win32.Console (generateConsoleCtrlEvent, cTRL_C_EVENT)
+import System.Win32.Process (getProcessId)
 #else
 import System.Posix.Signals hiding (killProcess)
 #endif
 
-#if defined(WINDOWS)
-
+#ifdef mingw32_HOST_OS
 stopProcess :: ProcessHandle -> IO ()
 stopProcess ph = do
-  pid <- getPid ph
-  stop <- case pid of
-    Nothing -> print "wtf"
-    Just pD -> do
-      putStrLn "Stop me, oh, stop me"
-      generateConsoleCtrlEvent cTRL_C_EVENT pD
+  pid <- getProcessId ph
+  putStrLn "Stop me, oh, stop me"
+  generateConsoleCtrlEvent cTRL_C_EVENT pid
 
 killProcess :: ProcessHandle -> IO ()
 killProcess ph = do
-  putStrLn "Murder most foul, as in the best it is, But this most foul, strange, and unnatural."
+  putStrLn "Murder most foul, as in the best it is, but this most foul, strange, and unnatural"
   terminateProcess ph
-
-
 #else
-
 stopProcess :: ProcessHandle -> IO ()
 stopProcess ph = do
   putStrLn "Stop me, oh, stop me"
@@ -50,8 +43,6 @@ killProcess ph = do
       OpenHandle h -> do
         putStrLn "Murder most foul, as in the best it is, But this most foul, strange, and unnatural."
         signalProcess sigKILL h
-        
-
 #endif
 
 shellReturnHandle :: String -> IO ProcessHandle
